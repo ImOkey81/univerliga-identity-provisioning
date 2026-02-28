21aeb96da2f1dc524e5dfb09c01261e9543b3144
IAmOkey <artem.to2018@yandex.ru>
2026-02-28 19:00:23 +0300

    Init commit

---
+++ src/test/java/org/example/backliga/BackLigaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backliga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackLigaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
